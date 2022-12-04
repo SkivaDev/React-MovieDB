ba4a8a4846e865c614744c51e767035e29f7bf5d
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-12-04 12:26:25 -0500

    Make Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.scss';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import MyRoutes from "./config/MyRoutes";
 
 function App() {
   return (
-    <div >
-      skiva
-    </div>
+    <BrowserRouter>
+        <Header />
+        <MyRoutes />
+        <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import "./Header.scss"
+import logo from "../../assets/tmovie.png"
+import { Link, useLocation } from 'react-router-dom'
+
+const headerNav = [
+  {
+    display: 'Home',
+    path: "/"
+  },
+  {
+    display: 'Movies',
+    path: "/movie"
+  },
+  {
+    display: 'TV Series',
+    path: "/tv"
+  },
+]
 
 function Header() {
+
+  const { pathname } = useLocation();
+  const headerRef = useRef(null);
+
+  const active = headerNav.findIndex(elem => elem.path === pathname);
+  console.log("activeEs: " , active);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            headerRef.current.classList.add('shrink');
+        } else {
+            headerRef.current.classList.remove('shrink');
+        }
+    }
+    window.addEventListener('scroll', shrinkHeader);
+    return () => {
+        window.removeEventListener('scroll', shrinkHeader);
+    };
+  }, []);  
+
   return (
-    <div>Header</div>
+    <div ref={headerRef} className='header'>
+      <div className='header__wrap container'>
+        <div className='logo'>
+          <img src={logo} alt="" />
+          <Link to='/'>tMovies</Link>
+        </div>
+        <ul className='header__nav'>
+          {
+            headerNav.map((e, i) => (
+              <li key={i} className={`${i === active}`}>
+                <Link to={e.path}>
+                  {e.display}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
